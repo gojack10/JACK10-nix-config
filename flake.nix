26a2e32ec7fb711a0ded4438057753b091a6d1bd
jack <gojack10@gmail.com>
2026-03-25 08:55:02 -0700

    style(foot): set fontSizeFoot to 11.5 for 10top
    CHANGES:
    
    - Add fontSizeFoot override of 11.5 for 10top machine

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
       machines = {
         # Linux hosts
         litetop = linuxDefaults // { fontSize = 9.5; fontSizeWaybar = 9.5; };
-        "10top" = linuxDefaults // { fontSizeWaybar = 11.5; };
+        "10top" = linuxDefaults // { fontSizeFoot = 11.5; fontSizeWaybar = 11.5; };
         desktop = linuxDefaults;
         # Darwin hosts
         m2-air = darwinDefaults;

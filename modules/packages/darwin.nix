66125e7b79bc3b7eab7e9b20b52a04d829725e49
gojack10 <gojack10@gmail.com>
2026-04-30 13:58:50 -0700

    guard fontconfig behind isLinux, simplify darwin font install

--- modules/packages/darwin.nix
+++ modules/packages/darwin.nix
@@ -1,30 +1,9 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
-
-{ pkgs, lib, ... }:
-
-let
-  # Symlink nerd fonts into ~/Library/Fonts so macOS apps can see them
-  fontSymlinks = font: name: file:
-    lib.nameValuePair
-      "Library/Fonts/${builtins.baseNameOf file}"
-      { source = "${font}/share/fonts/${name}/${builtins.baseNameOf file}"; };
-in
 {
   home.packages = with pkgs; [
-    # Fonts (just the ones you actually use on macOS)
+    # Fonts (nerd fonts install to nix profile; macOS apps can find them)
     nerd-fonts.jetbrains-mono
     nerd-fonts.symbols-only
   ];
-
-  # Symlink .ttf fonts into ~/Library/Fonts so they appear in all apps
-  home.file = lib.mkMerge [
-    (lib.listToAttrs (map (fontSymlinks pkgs.nerd-fonts.jetbrains-mono "JetBrainsMono")
-      (builtins.filter (f: lib.hasSuffix ".ttf" f)
-        (builtins.attrNames (builtins.readDir "${pkgs.nerd-fonts.jetbrains-mono}/share/fonts/JetBrainsMono")))))
-    (lib.listToAttrs (map (fontSymlinks pkgs.nerd-fonts.symbols-only "NerdFonts")
-      (builtins.filter (f: lib.hasSuffix ".ttf" f)
-        (builtins.attrNames (builtins.readDir "${pkgs.nerd-fonts.symbols-only}/share/fonts/NerdFonts")))))
-  ];
 }
-

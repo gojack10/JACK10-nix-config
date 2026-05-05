debfe35b77ee1e6f17f5af0580c1344b06dbcb05
jack <gojack10@gmail.com>
2026-05-05 12:15:49 -0700

    feat(packages): set force=true on all home.file entries
    CHANGES:
    
    - Add force = true to pi-setup, pi-update and hms file entries

--- modules/scripts.nix
+++ modules/scripts.nix
@@ -4,20 +4,24 @@
   home.file.".local/bin/tokens" = {
     source = ../scripts/tokens;
     executable = true;
+    force = true;
   };
 
   home.file.".local/bin/pi-setup" = {
     source = ../scripts/pi-setup;
     executable = true;
+    force = true;
   };
 
   home.file.".local/bin/pi-update" = {
     source = ../scripts/pi-update;
     executable = true;
+    force = true;
   };
 
   home.file.".local/bin/hms" = {
     source = ../scripts/hms;
     executable = true;
+    force = true;
   };
 }

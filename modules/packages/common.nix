592fb2abb9aeb1b713ffdf392ae74847c3745cc6
gojack10 <gojack10@gmail.com>
2026-05-08 20:22:02 -0700

    packages: install Hugging Face CLI via uv
    CHANGES:
    
    - Remove fragile dynamic nixpkgs package override
    
    - Install latest Hugging Face CLI through uv
    
    - Keep Python dependency resolution outside Nix

--- modules/packages/common.nix
+++ modules/packages/common.nix
@@ -1,11 +1,5 @@
 { config, pkgs, ... }:
 
-let
-  pypi-json = builtins.fetchurl {
-    url = "https://pypi.org/pypi/huggingface_hub/json";
-  };
-in
-
 {
   home.packages = with pkgs; [
     # Terminal & Shell
@@ -15,14 +9,6 @@ in
 
     # Dev tools
     fastfetch
-    (python3Packages.huggingface-hub.overrideAttrs (_: let
-      pypi = builtins.fromJSON (builtins.readFile pypi-json);
-    in rec {
-      version = pypi.info.version;
-      src = builtins.fetchurl {
-        url = "https://files.pythonhosted.org/packages/source/h/huggingface_hub/huggingface_hub-${version}.tar.gz";
-      };
-    }))
     htop
     neovim
     ripgrep

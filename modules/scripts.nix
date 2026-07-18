c3b43666d50c12826fea5057a644e8de387ca44b
jack <gojack10@gmail.com>
2026-07-17 19:29:08 -0700

    feat(tmux): add managed server restart command
    CHANGES:
    
    - Save all pane processes before replacing server
    - Restore sessions asynchronously with newly installed tmux

--- modules/scripts.nix
+++ modules/scripts.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, ... }: {
+{ config, lib, pkgs, ... }: {
   # Scripts sourced from ../scripts/ are managed by Home Manager and linked to ~/.local/bin.
   # Add new script entries below following the same pattern.
 
@@ -48,5 +48,33 @@
     source = ../scripts/deepwork-status;
     executable = true;
     force = true;
+  };
+
+  home.file.".local/bin/tmux-restart" = {
+    executable = true;
+    force = true;
+    text = ''
+      #!/bin/sh
+      set -eu
+
+      bin='${config.programs.tmux.package}/bin/tmux'
+      save='${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect/scripts/save.sh'
+      restore='${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect/scripts/restore.sh'
+      helper="__tmux_restore_$$"
+      log="$HOME/.cache/tmux-restart.log"
+
+      mkdir -p "$HOME/.cache"
+      "$bin" set-option -g @resurrect-processes ':all:'
+      "$save" quiet
+      nohup sh -c '
+        set -eu
+        sleep 1
+        TMUX= "$1" kill-server
+        TMUX= "$1" new-session -d -s "$3" \
+          "\"$1\" set-option -g @resurrect-processes :all:; exec \"$2\""
+      ' sh "$bin" "$restore" "$helper" >"$log" 2>&1 &
+
+      printf 'tmux saved; server will restart and restore. Reattach with: tmux attach\n'
+    '';
   };
 }
